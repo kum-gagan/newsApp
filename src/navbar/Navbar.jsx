430c836b67be4584f104d0969908af757a5cc526
kum-gagan <147703132+kum-gagan@users.noreply.github.com>
2024-06-23 12:04:05 +0530

    Update Navbar.jsx

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const Navbar = ({setSearch, setLoading, setCategory }) => {
+const Navbar = ({ setCountry, setSearch, setLoading, setCategory }) => {
 
     const newCategory = ['business', 'entertainment', 'health', 'science', 'sport', 'technology'];
+
+    const countryData = ['in','ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za'];
+
     const handleClick = (id) => {
         newCategory.map((elem, index) => {
             return (id === index) ? setCategory(elem) : null;
         })
+    }
+    const handleChange = (con) => {
+        setCountry(con);
     }
     return (
         <>
@@ -19,22 +25,31 @@ const Navbar = ({setSearch, setLoading, setCategory }) => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link pointer"  aria-current="page" onClick={() =>{setLoading('Loading...'); setCategory('general')}} href="/">All News</a>
+                                <a className="nav-link pointer" aria-current="page" onClick={() => { setLoading('Loading...'); setCategory('general') }} href="/">All News</a>
                             </li>
                             {
                                 newCategory.map((items, id) => {
                                     return <li className="nav-item" key={id}>
-                                        <a className="nav-link pointer text-capitalize" onClick={() =>{
-                                            setLoading('Loading...'); 
-                                            handleClick(id);}
-                                            }>
-                                             {items} </a>
+                                        <a className="nav-link pointer text-capitalize" onClick={() => {
+                                            setLoading('Loading...');
+                                            handleClick(id);
+                                        }
+                                        }>
+                                            {items} </a>
                                     </li>
                                 })
                             }
                         </ul>
                         <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" onChange={(e)=>setSearch(e.target.value)} placeholder="Search" aria-label="Search" />
+                            <select className="form-select text-uppercase"  onChange={(e) => setCountry(e.target.value)}>
+                                {
+                                    countryData.map((data, idx) => {
+                                        return (
+                                            <option className='text-uppercase' value={data} key={idx}>{data}</option>)
+                                    })
+                                }
+                            </select>
+                            <input className="form-control me-2" type="search" onChange={(e) => setSearch(e.target.value)} placeholder="Search" aria-label="Search" />
                         </form>
                     </div>
                 </div>
